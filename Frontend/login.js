ce3e0fcfa2c46c091caa0e8d58dfaac983185f02
vishalgouda486 <vishalgouda486@gmail.com>
2025-11-12 14:12:36 +0530

    fixed the fetch link

--- Frontend/login.js
+++ Frontend/login.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/login", {
+      const response = await fetch("https://prepmate-backend-x77z.onrender.com/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
